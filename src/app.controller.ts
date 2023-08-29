60dd5b1f9e99b3a805fd76e6e15f3ac89a63b4ea
felix lara <felixrlara00@gmail.com>
2023-08-28 22:09:39 -0400

    social login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,60 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller()
+
+// @Controller('')
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get()
+//   getHello(): string {
+//     return this.appService.getHello();
+//   }
+// }
+
+@Controller('auth')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req) {}
+
+  @Get('google-redirect')
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {
+    return this.appService.googleLogin(req)
   }
+
+  @Get("/facebook")
+  @UseGuards(AuthGuard("facebook"))
+  async facebookLogin(): Promise<any> {
+    return HttpStatus.OK;
+  }
+
+  @Get("/facebook/redirect")
+  @UseGuards(AuthGuard("facebook"))
+  async facebookLoginRedirect(@Req() req) {
+    return {
+      statusCode: HttpStatus.OK,
+      data: req.user,
+    };
+  }
+
+  @Get("/twitter")
+  @UseGuards(AuthGuard("twitter"))
+  async twitter(): Promise<any> {
+    return HttpStatus.OK;
+  }
+
+  @Get("/twitter/redirect")
+  @UseGuards(AuthGuard("twitter"))
+  async twitterLoginRedirect(@Req() req) {
+    return {
+      statusCode: HttpStatus.OK,
+      data: req.user,
+    };
+  }
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,13 @@ import { LotteryModule } from './lottery/lottery.module';
 import { UsersModule } from './users/users.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { DatabaseModule } from './database/database.module';
+import { GoogleStrategy } from './auth/google.strategy';
+import { FacebookStrategy } from './auth/facebook.strategy';
+import { TwitterStrategy } from './auth/twitter.strategy';
 
 @Module({
   imports: [LotteryModule, UsersModule, TicketsModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GoogleStrategy, FacebookStrategy, TwitterStrategy],
 })
 export class AppModule {}
